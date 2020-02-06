ede6f0d6c098c599e8d440bc1cdeeb99f7040ad2
lloarn <lloarn@penguin>
2020-02-06 10:35:20 +0000

    added neuron_mutate test

--- nn_testing.py
+++ nn_testing.py
@@ -8,18 +8,32 @@ def test_net_init():
     print("network name: %s " % net.name)
     print("project name: %s " % net.projectName)
     print("\n")
-
+    del net
 
 
 def test_neuron_init():
-        print("Testing neuron initialisation ")
-        neuron = nn.Neuron(5, [-999] * 5, "test_layer")
-        print("neuron: " + str(neuron))
-        print("neuron layer: %s " % neuron.layerName)
-        print("weighting 0: %s " % neuron.synaptic_weights[0])
-        print("\n")
+    print("Testing neuron initialisation ")
+    neuron = nn.Neuron(5, [-999] * 5, "test_layer")
+    print("neuron: " + str(neuron))
+    print("neuron layer: %s " % neuron.layerName)
+    print("weighting 0: %s " % neuron.synaptic_weights[0])
+    print("\n")
+    del neuron
+
+
+def test_neuron_mutate():
+    print("testing neuron mutation ")
+    neuron = nn.Neuron(5, [-999] * 5)
+    print("neuron: " + str(neuron))
+    print("weighting 0 before mutation: %s " % neuron.synaptic_weights[0])
+    neuron.mutate()
+    print("weighting 0 after mutation : %s " % neuron.synaptic_weights[0])
+    print("\n")
+    del neuron
+
 
 
 if __name__ == "__main__":
     test_neuron_init()
+    test_neuron_mutate()
     test_net_init()
