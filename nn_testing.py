85a011aa0a0c26d36d9cd1c7a6196e854d865777
lloarn <lloarn@penguin>
2020-02-06 10:41:26 +0000

    added neuron.think() test

--- nn_testing.py
+++ nn_testing.py
@@ -1,18 +1,8 @@
 import neural_network as nn
 
 
-def test_net_init():
-    print("Testing network initialisation ")
-    net = nn.NeuralNet([64, 42, 1], 1, False, "test_name", 1, "test_project")
-    print("network: " + str(net))
-    print("network name: %s " % net.name)
-    print("project name: %s " % net.projectName)
-    print("\n")
-    del net
-
-
 def test_neuron_init():
-    print("Testing neuron initialisation ")
+    print("testing neuron initialisation ")
     neuron = nn.Neuron(5, [-999] * 5, "test_layer")
     print("neuron: " + str(neuron))
     print("neuron layer: %s " % neuron.layerName)
@@ -32,8 +22,30 @@ def test_neuron_mutate():
     del neuron
 
 
+def test_neuron_think():
+    print("testing neuron think with two inputs ")
+    neuron = nn.Neuron(2, [-999] * 2)
+    print("neuron: " + str(neuron))
+    inputs = [1, -1]
+    print("inputs = %s " % str(inputs))
+    neuron.think(inputs)
+    print("output = %g " % neuron.output)
+    print("\n")
+    del neuron
+
+
+def test_net_init():
+    print("Testing network initialisation ")
+    net = nn.NeuralNet([64, 42, 1], 1, False, "test_name", 1, "test_project")
+    print("network: " + str(net))
+    print("network name: %s " % net.name)
+    print("project name: %s " % net.projectName)
+    print("\n")
+    del net
+
 
 if __name__ == "__main__":
     test_neuron_init()
     test_neuron_mutate()
+    test_neuron_think()
     test_net_init()
