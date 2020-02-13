81fe75d62bf79a159a60b07d37511d329e93821f
lloarn <lloarn@penguin>
2020-02-13 11:48:52 +0000

    added test for update weightings method

--- pong_test.py
+++ pong_test.py
@@ -1,8 +1,21 @@
 import pong_ai_v3 as pong
+import neural_network as nn
 
 
 def test_init_board():
-    pong.init_screen()
+    print("Test init screen function")
+    screen = pong.init_screen()
+    del screen
+    print("\n")
+
+
+def test_update():
+    print("test update_net_weightings sub")
+    net = nn.NeuralNet((6, 40, 20, 1), 1, False, "grad_descent_net", 0, pong.PROJECT_NAME)
+    inputArr, outputArr, expectedArr = pong.read_record_data(1)
+
+    pong.update_net_weightings(net, inputArr, expectedArr)
+    print("")
 
 
 if __name__ == "__main__":
