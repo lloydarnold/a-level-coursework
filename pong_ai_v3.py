d11f2851d828a83a748ee365c88180d56f68e247
lloarn <lloarn@penguin>
2020-02-24 12:56:39 +0000

    commented out redundant print statement (added for testing)

--- pong_ai_v3.py
+++ pong_ai_v3.py
@@ -389,7 +389,7 @@ def neural_net_move(network=nn.NeuralNet(), inputArray=None, bat=Bat(), ball=Bal
         for x in range(1, len(network.layers)):
             network.feed_forward(x)
         val = network.rtn_rating()
-        print(val)
+        # print(val)
 
         # set to use hyperbolic tangent function. could use any other logistic sigmoidal
         # function with range -1, 1 inclusive. Values > 0.3 mean move up. Values < -0.3 mean move down
