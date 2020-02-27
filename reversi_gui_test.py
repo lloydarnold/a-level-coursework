47e5cb023ca7fe908d929a4bf961664652e40027
lloarn <lloarn@penguin>
2020-02-27 17:24:56 +0000

    added test script for reversi_gui

---
+++ reversi_gui_test.py
@@ -0,0 +1,9 @@
+import reversi_gui_neuro_evolution as grev
+
+
+def test_init_board():
+    print("testing init board subroutine from graphical reversi")
+    grev.init_board()
+
+if __name__ == "__main":
+    test_init_board()
