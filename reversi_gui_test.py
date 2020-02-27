6d7ffdf08b6779287efd574b44c76b3ffa677634
lloarn <lloarn@penguin>
2020-02-27 18:19:20 +0000

    updated test routine

--- reversi_gui_test.py
+++ reversi_gui_test.py
@@ -1,9 +1,19 @@
 import reversi_gui_neuro_evolution as grev
+import pygame
+
+def create_screen():
+    gameSurface = pygame.display.set_mode((530, 530))
+    pygame.display.set_caption("Reversi")
+    pygame.mouse.set_visible(1)
+    gameSurface.fill((0, 255, 0))
+    return gameSurface
 
 
 def test_init_board():
     print("testing init board subroutine from graphical reversi")
-    grev.init_board()
+    grev.init_board(create_screen())
+    print("\n")
+
 
 if __name__ == "__main":
     test_init_board()
